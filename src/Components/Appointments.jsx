0fa2117c1cd625108e62bbd03810f62e077e6096
Michael Herr <michaelherr@Michaels-MacBook-Pro-2.local>
2021-07-13 09:35:37 -0400

    updating

--- src/Components/Appointments.jsx
+++ src/Components/Appointments.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "../App.css";
 import { Link } from "react-router-dom";
-import EditAppointment from "./EditAppointment";
 
 class Appointments extends Component {
   render() {
